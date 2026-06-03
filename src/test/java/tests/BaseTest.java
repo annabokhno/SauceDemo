a1eac24c62ac5e8c178de21489de500847b835f7
annabokhnowork-sudo <anna.bokhno.work@gmail.com>
2026-06-03 20:46:41 +0300

    added headless

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 import pages.*;
@@ -46,7 +47,8 @@ public class BaseTest {
             driver = new ChromeDriver(options);
 
         } else if (browser.equalsIgnoreCase("firefox")) {
-
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
             driver = new FirefoxDriver();
         }
 
@@ -80,6 +82,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
         return options;
     }
 
